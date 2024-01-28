7cc2d0ba71eed74073f5d8302c238abaada71075
lauta <altairdiaz245@gmail.com>
2024-01-28 02:47:30 -0300

    ob

--- init.lua
+++ init.lua
@@ -5,21 +5,9 @@ end
 require("plugins/init")
 require("teclas/teclas")
 require("opciones")
+
 require("config/diagnostic")
-
-local _border = "single"
-
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-	border = _border,
-})
-
-vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-	border = _border,
-})
-
-vim.diagnostic.config({
-	float = { border = _border },
-})
+require("config/diagnostic-show-border")
 
 for dir in io.popen([[ ls /home/lauta/.config/nvim/lua/plugins/configuracion/ ]]):lines() do
 	local a = dir:gsub(".lua", "")
