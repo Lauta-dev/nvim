aa0e704958bf25bcee0358dac5591522ea7e3022
lauta <altairdiaz245@gmail.com>
2024-02-27 17:19:16 -0300

    Opts cambiadas

--- lua/editor_config.lua
+++ lua/editor_config.lua
@@ -1,13 +1,11 @@
 local opt = vim.opt
-local api = vim.api
 local bg = vim.cmd.colorscheme
 local g = vim.g
-local lsp = vim.lsp
 local _borden = "single"
 
 bg("tokyonight-night")
-api.nvim_set_hl(0, "Normal", { bg = "none" })
-api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+--api.nvim_set_hl(0, "Normal", { bg = "none" })
+--api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
 g.loaded_netrw = 1
 g.loaded_netrwPlugin = 1
@@ -16,7 +14,7 @@ g.mapleader = " " -- Uso de la tecla espacio
 opt.hlsearch = false
 opt.incsearch = true
 
-opt.scrolloff = 10
+--opt.scrolloff = 10
 opt.signcolumn = "yes"
 
 opt.shortmess:append("sI")
@@ -34,7 +32,7 @@ opt.fileencoding = "utf-8" -- Codificacion utf-8
 opt.cursorline = true -- El cursor estara resaltado
 opt.clipboard = "unnamedplus" -- Habilitar portapapeles
 opt.termguicolors = true --
-opt.conceallevel = 2
+opt.conceallevel = 0
 
 vim.diagnostic.config({
 	update_in_insert = true,
