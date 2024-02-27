dfe435f711a596b94d7ad169e1d6eaba40fa3aed
lauta <altairdiaz245@gmail.com>
2024-02-27 17:19:29 -0300

    Nuevos atajos de teclado

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -1,21 +1,37 @@
 local key_map = vim.keymap.set
+local lsp_buf = vim.lsp.buf
 
+-- @param estado Estado del que esta el editor.
+-- @param tecla Combinación de teclas que se pulsaran.
+-- @param accion La acción a realizar.
 local function use_normal_key(estado, tecla, accion)
 	key_map(estado, "<" .. tecla .. ">", accion, { silent = true, noremap = true })
 end
 
+-- @param estado Estado del que esta el editor.
+-- @param tecla Combinación de teclas que se pulsaran.
+-- @param accion La acción a realizar.
 local function use_key_space(estado, tecla, accion)
 	key_map(estado, "<leader>" .. tecla, accion, { silent = true, noremap = true })
 end
 
+-- Ver ayuda
+use_key_space("n", "lh", lsp_buf.signature_help)
+
+-- Simular un hover
+use_key_space("n", "hh", lsp_buf.hover)
+
+-- Ir a la definición de un método o funcion
+use_key_space("n", "ll", vim.lsp.buf.definition)
+
 -- Uso de rest
-use_key_space("n", "e", "<Plug>RestNvim")
+use_key_space("n", "ee", "<Plug>RestNvim")
 
 -- Ver el dianostico
 use_normal_key("n", "C-l", vim.diagnostic.open_float)
 
 use_normal_key("n", "C-s", ":w!<CR>") -- Guardar los cambios
-use_normal_key("n", "C-x", ":q!<CR>") -- Cerrar el archivo sin guardar
+use_key_space("n", "xx", ":q!<CR>") -- Cerrar el archivo sin guardar
 
 -- Explorador de archivos
 use_normal_key("n", "C-b", ":NvimTreeToggle <CR>") -- Abrir y cierra
@@ -48,11 +64,7 @@ use_normal_key("n", "C-j", ":ToggleTerm<CR>")
 use_normal_key("t", "C-j", ":wincdmd ToggleTerm<CR>")
 
 -- Renombrar variables
-use_normal_key("n", "C-p", ":vim.lsp.buf.rename()<CR>")
-
-local api = require("Comment.api")
---vim.keymap.set("n", "gc", api.call("toggle.linewise", "g@"), { expr = true })
-vim.keymap.set("n", "<C-.>", api.call("toggle.linewise", "g@$"), { expr = true })
+use_normal_key("n", "C-p", vim.lsp.buf.rename)
 
 -- Formatear texto al guardar
 local augroup = vim.api.nvim_create_augroup
@@ -60,10 +72,11 @@ local autocmd = vim.api.nvim_create_autocmd
 augroup("__formatter__", { clear = true })
 autocmd("BufWritePost", {
 	group = "__formatter__",
+	--command = ":lua vim.lsp.buf.format()",
 	command = ":FormatWrite",
 })
 
 -- Telescope
 use_key_space("n", "ff", ":Telescope find_files<CR>")
 use_key_space("n", "fb", ":Telescope buffers<CR>")
-use_key_space("n", "cs", ":Telescope colorscheme<CR>")
+use_key_space("n", "fr", ":Telescope lsp_references<CR>")
