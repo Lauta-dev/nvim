20653bb9a96438f179094a398e5bf253cf3217e2
lauta-dev <altairdiaz245@gmail.com>
2024-09-18 16:47:56 -0300

    autocmd for format code

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -1,43 +1,7 @@
-vim.api.nvim_create_augroup("AutoFormat", {})
-local mason_bin_folder = os.getenv("HOME") .. "/.local/share/PersonalVim/mason/bin/"
-
-local current_buffer = vim.api.nvim_eval("expand('%:p')")
-
-local function autocmd(pattern, cmd, args)
-	local args_str = table.concat(args, " ")
-
-	vim.api.nvim_create_autocmd("BufWritePost", {
-		pattern = pattern,
-		group = "AutoFormat",
-		callback = function()
-			vim.cmd("silent !" .. cmd .. " " .. args_str .. " " .. current_buffer)
-		end,
-	})
-end
-
-local list = {
-	{
-		pattern = "*.lua",
-		formatter = "stylua",
-		args = {},
-	},
-	{
-		pattern = "*.cs",
-		formatter = "dotnet-csharpier",
-		args = { "--fast" },
-	},
-	{
-		pattern = { "*.js", "*.ts", "*.json", "*.jsx" },
-		formatter = "biome",
-		args = { "format", "--write" },
-	},
-	{
-		pattern = { "*.html", "*.css", "*.yaml", "*.yml" },
-		formatter = "prettier",
-		args = { "--write" },
-	},
-}
-
-for _, v in ipairs(list) do
-	autocmd(v.pattern, mason_bin_folder .. v.formatter, v.args)
-end
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+augroup("__formatter__", { clear = true })
+autocmd("BufWritePost", {
+	group = "__formatter__",
+	command = ":FormatWrite",
+})
