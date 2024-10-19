874dde61879c320fd7f4e6dddb420cb8596a79be
lauta-dev <altairdiaz123@gmail.com>
2024-10-18 22:42:00 -0300

    +

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,140 +1,149 @@
 local n = {
-	"plugins/autocomplete",
-	"plugins/lsp",
-	"plugins/themes",
-	"plugins/ia",
+  "plugins/autocomplete",
+  "plugins/lsp",
+  "plugins/themes",
+  "plugins/ia",
+  "plugins/SchemaStore",
+  "plugins/garbage_day"
 }
 
 local plugins = {
-	{
-		"folke/todo-comments.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = true,
-		cmd = { "TodoTelescope" },
-	},
-  { 'mistweaverco/kulala.nvim', opts = {} },
-
-	{
-		"stevearc/conform.nvim",
-		config = function()
-			require("conform").setup({
-				formatters_by_ft = {
-					lua = { "stylua" },
-					javascript = { "biome" },
-					typescript = { "biome" },
-					javascriptreact = { "biome" },
-					typescriptreact = { "biome" },
-					json = { "biome" },
-				},
-          format_on_save = {
-    timeout_ms = 500,
-    lsp_format = "fallback",
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = true,
+    cmd = { "TodoTelescope" },
   },
-			})
-		end,
-	},
+  {
+    'mistweaverco/kulala.nvim',
+    opts = {},
+    ft = { "http" }
+  },
 
-	{
-		"j-hui/fidget.nvim",
-		tag = "v1.0.0",
-		opts = {},
-	},
+  {
+    "stevearc/conform.nvim",
+    config = function()
+      require("conform").setup({
+        formatters_by_ft = {
+          lua = { "stylua" },
+          javascript = { "biome" },
+          typescript = { "biome" },
+          javascriptreact = { "biome" },
+          typescriptreact = { "biome" },
+          json = { "biome" },
+        },
+        format_on_save = {
+          timeout_ms = 500,
+          lsp_format = "fallback",
+        },
+      })
+    end,
+    ft = { "lua", "javascript", "javascriptreact", "typescriptreact", "typescript", "json" }
+  },
 
-	{ "nvim-tree/nvim-web-devicons" },
-	{
-		"windwp/nvim-autopairs",
-		event = "InsertEnter",
-		config = true,
-	},
-	{
-		"nvim-lualine/lualine.nvim",
-		config = function()
-			local config = require("plugins.configs.lualine_conf")
-			require("lualine").setup(config)
-		end,
-	},
+  {
+    "j-hui/fidget.nvim",
+    tag = "v1.0.0",
+    opts = {},
+  },
 
-	{
-		"nvim-treesitter/nvim-treesitter",
-		config = function()
-			local config = require("plugins.configs.nvim-treesitter")
-			require("nvim-treesitter.configs").setup(config)
-		end,
-	},
+  { "nvim-tree/nvim-web-devicons" },
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = true,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    config = function()
+      local config = require("plugins.configs.lualine_conf")
+      require("lualine").setup(config)
+    end,
+  },
 
-	{
-		"nvim-tree/nvim-tree.lua",
-		config = true,
-	},
+  {
+    "nvim-treesitter/nvim-treesitter",
+    config = function()
+      local config = require("plugins.configs.nvim-treesitter")
+      require("nvim-treesitter.configs").setup(config)
+    end,
+  },
 
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.5",
+  {
+    "nvim-tree/nvim-tree.lua",
+    config = true,
+  },
 
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"tsakirist/telescope-lazy.nvim",
-		},
-	},
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.5",
 
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		config = function()
-			local hooks = require("ibl.hooks")
-			local config = require("plugins.configs.indent-blankline")
-			config.load_rainbow(hooks)
-			require("ibl").setup({ scope = { highlight = config.highlight } })
-		end,
-	},
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "tsakirist/telescope-lazy.nvim",
+    },
+  },
 
-	{
-		"lewis6991/gitsigns.nvim",
-		config = function()
-			require("gitsigns").setup(require("plugins.configs.gitsigns-nvim"))
-		end,
-	},
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    config = function()
+      local hooks = require("ibl.hooks")
+      local config = require("plugins.configs.indent-blankline")
+      config.load_rainbow(hooks)
+      require("ibl").setup({ scope = { highlight = config.highlight } })
+    end,
+  },
 
-	{
-		"williamboman/mason.nvim",
-		cmd = {
-			"Mason",
-			"MasonInstall",
-			"MasonUninstall",
-			"MasonUninstallAll",
-			"MasonLog",
-		},
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup(require("plugins.configs.gitsigns-nvim"))
+    end,
+  },
 
-		config = true,
-	},
+  {
+    "williamboman/mason.nvim",
+    cmd = {
+      "Mason",
+      "MasonInstall",
+      "MasonUninstall",
+      "MasonUninstallAll",
+      "MasonLog",
+    },
 
-	{
-		"mfussenegger/nvim-lint",
-		config = function()
-			require("lint").linters_by_ft = {
-				cs = { "ast-grep" },
-				html = { "htmlhint" },
-				css = { "stylelint" },
-				javascript = { "biome" },
-				typescript = { "biome" },
-				javascriptreact = { "biome" },
-				typescriptreact = { "biome" },
-			}
-		end,
-	},
+    config = true,
+  },
 
-	{
-		"nvim-lua/plenary.nvim",
-		cmd = { "PlenaryBustedFile", "PlenaryBustedDirectory" },
-		config = function()
-			require("plenary.async")
-		end,
-	},
+  {
+    "mfussenegger/nvim-lint",
+    config = function()
+      require("lint").linters_by_ft = {
+        cs = { "ast-grep" },
+        html = { "htmlhint" },
+        css = { "stylelint" },
+        javascript = { "biome" },
+        typescript = { "biome" },
+        javascriptreact = { "biome" },
+        typescriptreact = { "biome" },
+        json = { "biome" }
+      }
+    end,
+    ft = { "cs", "html", "css", "javascriptreact", "javascript", "typescriptreact", "typescript" }
+  },
+
+  {
+    "nvim-lua/plenary.nvim",
+    cmd = { "PlenaryBustedFile", "PlenaryBustedDirectory" },
+    config = function()
+      require("plenary.async")
+    end,
+  },
 }
 
 for _, v in ipairs(n) do
-	for _, d in ipairs(require(v)) do
-		table.insert(plugins, d)
-	end
+  for _, d in ipairs(require(v)) do
+    table.insert(plugins, d)
+  end
 end
 
 require("lazy").setup(plugins)
