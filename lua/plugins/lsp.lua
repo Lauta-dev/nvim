8c13338f49c71e5b9288f8ba040d3fc002b4b1bb
lauta-dev <altairdiaz245@gmail.com>
2025-08-21 22:35:22 -0300

    Cambio de config a la nueva

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -12,133 +12,49 @@ return {
     event = { "BufReadPre", "BufNewFile" },
 
     config = function()
-      local lsp = require("lspconfig")
       --local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local capabilities = require('blink.cmp').get_lsp_capabilities()
 
+      local lsp = vim.lsp
+      local capabilities = require('blink.cmp').get_lsp_capabilities()
       capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-      local homepath = os.getenv("HOME")
-
-      local angular_cmd = {
-        homepath
-        ..
-        "/.local/share/lauta_dev_nvim/mason/packages/angular-language-server/node_modules/@angular/language-server/bin/ngserver",
-        "--stdio",
-        "--tsProbeLocations",
-        homepath
-        .. "/.local/share/lauta_dev_nvim/mason/packages/angular-language-server/node_modules/typescript/lib",
-        "--ngProbeLocations",
-        homepath
-        .. "/.local/share/lauta_dev_nvim/mason/packages/angular-language-server/node_modules/@angular/language-server/",
+      local servers = {
+        go = "gopls",
+        ts_ls = "ts_ls",
+        bash = "bashls",
+        lua = "lua_ls",
+        html = "html",
+        css = "cssls",
+        css_module = "cssmodules_ls",
+        json = "jsonls",
+        yaml = "yamlls"
       }
 
+      local serversWithScheme = {
+        json = "jsonls",
+        yaml = "yamlls"
+      }
 
-      -- GO
-      lsp.gopls.setup({ capabilities = capabilities })
+      --- @param lang string Lenguaje
+      local function add_schema(lang)
+        return {
+          [lang] = {
+            schemas = require("schemastore")[lang].schemas(),
+            validate = { enable = true },
+          },
+        }
+      end
 
-      -- HTML
-      lsp.html.setup({ capabilities = capabilities })
+      for lang, server in pairs(servers) do
+        local config = { capabilities = capabilities }
 
-      -- CSS
-      lsp.cssls.setup({ capabilities = capabilities })
-
-      -- Angular
-      lsp.angularls.setup({
-        capabilities = capabilities,
-        cmd = angular_cmd,
-        on_new_config = function(new_config)
-          new_config.cmd = angular_cmd
-        end,
-      })
-
-      -- TailwindCSS
-      lsp.tailwindcss.setup({
-        filetypes = {
-          "astro",
-          "javascriptreact",
-          "typescriptreact",
-        },
-        capabilities = capabilities,
-      })
-
-      -- JS/TS/JSX/TSX
-      lsp.ts_ls.setup({ capabilities = capabilities })
-
-      -- Docker
-      lsp.docker_compose_language_service.setup({ capabilities = capabilities })
-      lsp.dockerls.setup({ capabilities = capabilities })
-
-      -- BASH (Linux)
-      lsp.bashls.setup({ capabilities = capabilities })
-
-      -- Lua (Linux)
-      lsp.lua_ls.setup({
-        on_init = function(client)
-          if client.workspace_folders then
-            local path = client.workspace_folders[1].name
-            if vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc") then
-              return
-            end
-          end
-
-          client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
-            runtime = {
-              version = "LuaJIT",
-            },
-            workspace = {
-              checkThirdParty = false,
-              library = {
-                vim.env.VIMRUNTIME,
-              },
-            },
-          })
-        end,
-        settings = {
-          Lua = {},
-        },
-      })
-
-      -- Python
-      lsp.pyright.setup({ capabilities = capabilities })
-
-      -- C#
-      lsp.omnisharp.setup(require("plugins.configs.lsp.csharp_omnisharp"))
-
-      -- PHP
-      lsp.intelephense.setup({ capabilities = capabilities })
-
-      -- JSON
-      vim.api.nvim_create_autocmd("BufEnter", {
-        pattern = "*.json",
-        callback = function(args)
-          lsp.jsonls.setup({
-            capabilities = capabilities,
-            settings = {
-              json = {
-                schemas = require("schemastore").json.schemas(),
-                validate = { enable = true },
-              },
-            },
-          })
+        if serversWithScheme[lang] then
+          config.settings = add_schema(lang)
         end
-      })
 
-      -- YAML/YML
-      vim.api.nvim_create_autocmd("BufEnter", {
-        pattern = "*.yml",
-        callback = function(args)
-          lsp.yamlls.setup({
-            capabilities = capabilities,
-            settings = {
-              yaml = {
-                schemas = require("schemastore").yaml.schemas(),
-                validate = { enable = true },
-              },
-            },
-          })
-        end
-      })
+        lsp.config(server, config)
+        lsp.enable(server)
+      end
     end,
   },
 }
