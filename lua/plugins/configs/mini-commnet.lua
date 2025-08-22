91490f802f62d921994190a0daea537168d053fc
lauta-dev <altairdiaz245@gmail.com>
2025-08-21 22:40:16 -0300

    Config para comentarios

---
+++ lua/plugins/configs/mini-commnet.lua
@@ -0,0 +1,9 @@
+return {
+  -- Module mappings. Use `''` (empty string) to disable one.
+  mappings = {
+    comment = '',
+    comment_line = 'gc',
+    comment_visual = 'gc',
+    textobject = '',
+  }
+}
